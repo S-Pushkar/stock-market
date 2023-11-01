a1472a7e355475d5e147e874b65e34bafebbd504
Pushkar S <spushkar0071234@gmail.com>
2023-11-01 20:39:52 +0530

    Made target = _blank for a tags.

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
 	return (
 		<PageContext.Provider value={{log, setLog, currentPage, setCurrentPage}}>
 			<div className='min-h-screen flex flex-col justify-center'>
-				<header className='sticky top-0 flex flex-col justify-center bg-[#000000]'>
+				<header className='sticky top-0 w-full flex flex-col justify-center bg-[#000000]'>
 					<Link to="/">
 						<h1 className='text-[rgb(77,212,106)] text-s lg:text-3xl text-center font-bold'>Stocks</h1>
 					</Link>
@@ -39,7 +39,7 @@ function App() {
 							<div className='lg:px-6 px-2 bg-[rgb(64,64,64)] border-2 border-solid border-black rounded-xl'>{log}</div>
 						</Link>
 					</nav>
-					<hr />
+					<hr className='w-full'/>
 				</header>
 				<main className='flex-grow'>
 					<Routes>
@@ -54,15 +54,15 @@ function App() {
 				</main>
 				<div className='mb-0 w-full mt-4'>
 					<hr />
-				<footer className='flex justify-start bg-[#000000]'>
-					<div className={`foot-stuff`}>
-						<Link className={`link ${'sm:text-xs lg:text-lg ml-8 mr-8 mb-8'}`} to="/about"><img className={`aboutimg`} src={AboutImg} alt="about" /></Link>
-						<a className={`imgs ${'sm:text-xs lg:text-lg ml-8 mr-8 mb-8'}`} href="https://www.facebook.com/"><img className={`fbimg`} src={Facebook} alt="Facebook" /></a>
-						<a className={`imgs ${'sm:text-xs lg:text-lg ml-8 mr-8 mb-8'}`} href="https://www.instagram.com/"><img className={`igimg`} src={InstaImg} alt="Instagram" /></a>
-						<a className={`imgs ${'sm:text-xs lg:text-lg ml-8 mr-8 mb-8'}`} href="https://in.linkedin.com/"><img className={`inimg`} src={LinImg} alt="Facebook" /></a>
-						<a className={`imgs ${'sm:text-xs lg:text-lg ml-8 mr-8 mb-8'}`} href="https://twitter.com/"><img className={`ximg`} src={TwitImg} alt="Facebook" /></a>
-					</div>
-				</footer>
+					<footer className='flex justify-start'>
+						<div className={`foot-stuff`}>
+							<Link className={`link ${'sm:text-xs lg:text-lg ml-8 mr-8 mb-8'}`} to="/about"><img className={`aboutimg`} src={AboutImg} alt="about" /></Link>
+							<a className={`imgs ${'sm:text-xs lg:text-lg ml-8 mr-8 mb-8'}`} target = "_blank" href="https://www.facebook.com/"><img className={`fbimg`} src={Facebook} alt="Facebook" /></a>
+							<a className={`imgs ${'sm:text-xs lg:text-lg ml-8 mr-8 mb-8'}`} target = "_blank" href="https://www.instagram.com/"><img className={`igimg`} src={InstaImg} alt="Instagram" /></a>
+							<a className={`imgs ${'sm:text-xs lg:text-lg ml-8 mr-8 mb-8'}`} target = "_blank" href="https://in.linkedin.com/"><img className={`inimg`} src={LinImg} alt="Facebook" /></a>
+							<a className={`imgs ${'sm:text-xs lg:text-lg ml-8 mr-8 mb-8'}`} target = "_blank" href="https://twitter.com/"><img className={`ximg`} src={TwitImg} alt="Facebook" /></a>
+						</div>
+					</footer>
 				</div>
 			</div>
 		</PageContext.Provider>
