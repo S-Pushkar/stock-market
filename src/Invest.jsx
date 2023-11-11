3a8d30e88905ff4ca213b4ef372bba0dd5802af3
pushkarpurs <pushkar.p.urs@gmail.com>
2023-11-11 12:48:41 +0530

    sell buttons

--- src/Invest.jsx
+++ src/Invest.jsx
@@ -35,6 +35,7 @@ export default function Invest() {
 						<div className={'inDisp'} style={{color: stockChange[0]>=0? 'green':'red',display:'block',}}>&nbsp;{stockPerc[0]}&nbsp;{stockSymbol[0]}&nbsp;{stockName[0]}&nbsp; at &nbsp;{stockPrice[0]}$&nbsp;Change&nbsp;{stockChange[0]}$&nbsp;</div>
 						</div>
 						<button className="bg-[rgb(227,223,223)] m-3 text-black px-1 py-1 hover:bg-gray" onClick={() => buyShares()}>BUY</button>
+						<button className="bg-[rgb(227,223,223)] m-3 text-black px-1 py-1 hover:bg-gray" onClick={() => sellShares()}>SELL</button>
 					</div>
 					<br/>
 					<div className={`StkDisp ${"rounded-lg flex justify-center items-center"}`}>
@@ -43,6 +44,7 @@ export default function Invest() {
 						<div className={'inDisp'} style={{color: stockChange[1]>=0? 'green':'red',display:'block',}}>&nbsp;{stockPerc[1]}&nbsp;{stockSymbol[1]}&nbsp;{stockName[1]}&nbsp; at &nbsp;{stockPrice[1]}₹&nbsp;Change&nbsp;{stockChange[1]}₹&nbsp;</div>
 						</div>
 						<button className="bg-[rgb(227,223,223)] m-3 text-black px-1 py-1 hover:bg-gray" onClick={() => buyShares()}>BUY</button>
+						<button className="bg-[rgb(227,223,223)] m-3 text-black px-1 py-1 hover:bg-gray" onClick={() => sellShares()}>SELL</button>
 					</div>
 					<br/>
 					<div className={`StkDisp ${"rounded-lg flex justify-center items-center"}`}>
@@ -51,6 +53,7 @@ export default function Invest() {
 						<div className={'inDisp'} style={{color: stockChange[2]>=0? 'green':'red',display:'block',}}>&nbsp;{stockPerc[2]}&nbsp;{stockSymbol[2]}&nbsp;{stockName[2]}&nbsp; at &nbsp;{stockPrice[2]}$&nbsp;Change&nbsp;{stockChange[2]}$&nbsp;</div>
 						</div>
 						<button className="bg-[rgb(227,223,223)] m-3 text-black px-1 py-1 hover:bg-gray" onClick={() => buyShares()}>BUY</button>
+						<button className="bg-[rgb(227,223,223)] m-3 text-black px-1 py-1 hover:bg-gray" onClick={() => sellShares()}>SELL</button>
 					</div>
 					<br/>
 					<div className={`StkDisp ${"rounded-lg flex justify-center items-center"}`}>
@@ -59,6 +62,7 @@ export default function Invest() {
 						<div className={'inDisp'} style={{color: stockChange[3]>=0? 'green':'red',display:'block',}}>&nbsp;{stockPerc[3]}&nbsp;{stockSymbol[3]}&nbsp;{stockName[3]}&nbsp; at &nbsp;{stockPrice[3]}$&nbsp;Change&nbsp;{stockChange[3]}$&nbsp;</div>
 						</div>
 						<button className="bg-[rgb(227,223,223)] m-3 text-black px-1 py-1 hover:bg-gray" onClick={() => buyShares()}>BUY</button>
+						<button className="bg-[rgb(227,223,223)] m-3 text-black px-1 py-1 hover:bg-gray" onClick={() => sellShares()}>SELL</button>
 					</div>
 					<br/>
 					<div className={`StkDisp ${"rounded-lg flex justify-center items-center"}`}>
@@ -67,6 +71,7 @@ export default function Invest() {
 						<div className={'inDisp'} style={{color: stockChange[4]>=0? 'green':'red',display:'block',}}>&nbsp;{stockPerc[4]}&nbsp;{stockSymbol[4]}&nbsp;{stockName[4]}&nbsp; at &nbsp;{stockPrice[4]}₹&nbsp;Change&nbsp;{stockChange[4]}₹&nbsp;</div>
 						</div>
 						<button className="bg-[rgb(227,223,223)] m-3 text-black px-1 py-1 hover:bg-gray" onClick={() => buyShares()}>BUY</button>
+						<button className="bg-[rgb(227,223,223)] m-3 text-black px-1 py-1 hover:bg-gray" onClick={() => sellShares()}>SELL</button>
 					</div>
 					<br/>
 					<div className={`StkDisp ${"rounded-lg flex justify-center items-center"}`}>
@@ -75,6 +80,7 @@ export default function Invest() {
 						<div className={'inDisp'} style={{color: stockChange[5]>=0? 'green':'red',display:'block',}}>&nbsp;{stockPerc[5]}&nbsp;{stockSymbol[5]}&nbsp;{stockName[5]}&nbsp; at &nbsp;{stockPrice[5]}₹&nbsp;Change&nbsp;{stockChange[5]}₹&nbsp;</div>
 						</div>
 						<button className="bg-[rgb(227,223,223)] m-3 text-black px-1 py-1 hover:bg-gray" onClick={() => buyShares()}>BUY</button>
+						<button className="bg-[rgb(227,223,223)] m-3 text-black px-1 py-1 hover:bg-gray" onClick={() => sellShares()}>SELL</button>
 					</div>
 					<br/>
 					<div className={`StkDisp ${"rounded-lg flex justify-center items-center"}`}>
@@ -83,6 +89,7 @@ export default function Invest() {
 						<div className={'inDisp'} style={{color: stockChange[6]>=0? 'green':'red',display:'block',}}>&nbsp;{stockPerc[6]}&nbsp;{stockSymbol[6]}&nbsp;{stockName[6]}&nbsp; at &nbsp;{stockPrice[6]}$&nbsp;Change&nbsp;{stockChange[6]}$&nbsp;</div>
 						</div>
 						<button className="bg-[rgb(227,223,223)] m-3 text-black px-1 py-1 hover:bg-gray" onClick={() => buyShares()}>BUY</button>
+						<button className="bg-[rgb(227,223,223)] m-3 text-black px-1 py-1 hover:bg-gray" onClick={() => sellShares()}>SELL</button>
 					</div>
 					<br/>
 					<div className={`StkDisp ${"rounded-lg flex justify-center items-center"}`}>
@@ -91,6 +98,7 @@ export default function Invest() {
 						<div className={'inDisp'} style={{color: stockChange[7]>=0? 'green':'red',display:'block',}}>&nbsp;{stockPerc[7]}&nbsp;{stockSymbol[7]}&nbsp;{stockName[7]}&nbsp; at &nbsp;{stockPrice[7]}$&nbsp;Change&nbsp;{stockChange[7]}$&nbsp;</div>
 						</div>
 						<button className="bg-[rgb(227,223,223)] m-3 text-black px-1 py-1 hover:bg-gray" onClick={() => buyShares()}>BUY</button>
+						<button className="bg-[rgb(227,223,223)] m-3 text-black px-1 py-1 hover:bg-gray" onClick={() => sellShares()}>SELL</button>
 					</div>
             </div>
         </div>
