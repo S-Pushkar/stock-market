645c45c1c6cb6bf1ea2cdbfec4a3c2ea11e2471b
pushkarpurs <pushkar.p.urs@gmail.com>
2023-11-15 15:32:14 +0530

    about improvements

--- src/About.jsx
+++ src/About.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { PageContext } from './App';
 import AboutImg from './assets/AboutImg.svg';
+import './App.css';
 
 export default function About() {
     const {log, setLog, currentPage, setCurrentPage} = useContext(PageContext);
@@ -11,7 +12,7 @@ export default function About() {
         <div className="lg:px-20 px-8 md:text-base text-xs lg:text-base">
             <h1 className="text-[rgb(212,195,87)] mt-4 mb-2 text-sm lg:text-lg font-bold">ABOUT</h1>
            <div className="grid grid-cols-2 grid-rows-3 min-h-screen mt-8 mb-8">
-				<div className="bg-grey-400 p-4 rounded-lg flex justify-center items-center"
+				<div className={`profimg ${'bg-grey-400 p-4 rounded-lg flex justify-center items-center'}`}
 					style={{
 					backgroundImage: `url(${AboutImg})`,
 					height:'20em',
@@ -21,19 +22,19 @@ export default function About() {
 					backgroundRepeat: 'no-repeat',
 					}}>
 				</div>
-				<div className="bg-grey-400 p-4 prose lg:prose-xl prose-p:text-grey-900  hover:text-white rounded-xl flex justify-center items-center">
+				<div className={`abtxt ${'bg-grey-400 p-4 prose lg:prose-xl prose-p:text-grey-900  hover:text-white rounded-xl flex justify-center'}`}>
 					<p className="text-center leading-relaxed">
-					<h1 style={{color:'white',}}>Priyanshu Sachan</h1>
+					<h2 style={{color:'white',}}>Priyanshu Sachan</h2>
 						About Priyanshu Sachan
 					</p>
 				</div>
-				<div className="bg-grey-400 p-4 prose lg:prose-xl prose-p:text-grey-900  hover:text-white rounded-xl flex justify-center items-center">
+				<div className={`abtxt ${'bg-grey-400 p-4 prose lg:prose-xl prose-p:text-grey-900  hover:text-white rounded-xl flex justify-center'}`}>
 					<p className="text-center leading-relaxed">
-					<h1 style={{color:'white',}}>Pushkar P Urs</h1>
+					<h2 style={{color:'white',}}>Pushkar P Urs</h2>
 						About Pushkar Urs
 					</p>
 				</div>
-				<div className="bg-grey-400 p-4 rounded-lg flex justify-center items-center"
+				<div className={`profimg ${'bg-grey-400 p-4 rounded-lg flex justify-center items-center'}`}
 					style={{
 					backgroundImage: `url(${AboutImg})`,
 					height:'20em',
@@ -43,7 +44,7 @@ export default function About() {
 					backgroundRepeat: 'no-repeat',
 					}}>
 				</div>
-				<div className="bg-grey-400 p-4 rounded-lg flex justify-center items-center"
+				<div className={`profimg ${'bg-grey-400 p-4 rounded-lg flex justify-center items-center'}`}
 					style={{
 					backgroundImage: `url(${AboutImg})`,
 					height:'20em',
@@ -53,9 +54,9 @@ export default function About() {
 					backgroundRepeat: 'no-repeat',
 					}}>
 				</div>
-				<div className="bg-grey-400 p-4 prose lg:prose-xl prose-p:text-grey-900  hover:text-white rounded-xl flex justify-center items-center">
+				<div className={`abtxt ${'bg-grey-400 p-4 prose lg:prose-xl prose-p:text-grey-900  hover:text-white rounded-xl flex justify-center'}`}>
 					<p className="text-center leading-relaxed">
-					<h1 style={{color:'white',}}>Pushkar S</h1>
+					<h2 style={{color:'white',}}>Pushkar S</h2>
 						About Pushkar S
 					</p>
 				</div>
